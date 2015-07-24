7da6e0280e19726c50f97b27d79f235bc5cf5d9c
dpicard31 <casper__07@hotmail.com>
2015-07-24 15:09:43 -0400

    Prog Assn 2
    Two functions: one converts a matrix to a list, and the other allows for
    caching/retrieving the inverse of the first function.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix will create a matrix object that can cache its inverse
+## cacheSolve will compute the inverse of makeCacheMatrix, or retrieve from 
+## cache if it already has been computed
 
-## Write a short comment describing this function
+## makeCacheMatrix will create a matrix object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinv <- function(solve) m <<- solve
+  getinv <- function() m
+  list(set=set, get=get, setinv=setinv, getinv=getinv)
 }
 
-
-## Write a short comment describing this function
+## Returns a matrix that is the inverse of 'x' , checks cache first
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinv()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data)
+  x$setinv(m)
+  m
 }
